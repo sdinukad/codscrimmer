2b4c458c582c4911bd1485af35b5bf54e7ffdf39
Dinuka Dangampalage <sdangampalage@deakin.edu.au>
2023-05-25 18:42:12 +1000

    initial code for login page

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const hbs = require("hbs")
+const collection=require("./mongodb")
+
+const templatePath=path.join(__dirname,'../templates')
+
+// Specify the path to the public directory
+const publicPath = path.join(__dirname, "../public"); 
+
+
+app.use(express.json())
+app.set("view engine","hbs")
+app.set("views", templatePath)
+app.use(express.urlencoded({extended:false}))
+//serve css
+app.use(express.static(publicPath));
+
+app.get("/", (req,res)=>{
+    res.render("login");
+})
+
+app.get("/signup", (req,res)=>{
+    res.render("signup");
+})
+
+app.post("/signup",async (req,res)=>{
+    const data={
+        name:req.body.name,
+        password:req.body.password
+    }
+
+    await collection.insertMany([data])
+
+    res.render("home")
+})
+
+app.post("/login",async (req,res)=>{
+
+    try{
+        const check=await collection.findOne({name:req.body.name})
+        if(check.password===req.body.password){
+            res.render("home");
+        }
+        else{
+            res.send("Password is incorrect")
+        }
+    }
+    catch{
+        res.send("Username or password does not exist")
+    }
+    
+})
+
+app.listen(3000, ()=>{
+    console.log("port connected 3000");
+})
