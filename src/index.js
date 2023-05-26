8df9d41b1df6c0fdcccbc63e2d62ef0160cc5ffd
Dinuka Dangampalage <sdangampalage@deakin.edu.au>
2023-05-26 20:01:34 +1000

    express sessions implemented succesfully. added logout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require("express")
-const app = express()
+const session = require("express-session");
 const path = require("path")
 const hbs = require("hbs")
 const collection=require("./mongodb")
+
+
+const app = express()
+
+
 //const PORT = process.env.PORT || 3030; //https://www.freecodecamp.org/news/how-to-deploy-nodejs-application-with-render/
 const PORT = 3000; //port for local testing
 const templatePath=path.join(__dirname,'../templates')
@@ -10,7 +15,16 @@ const templatePath=path.join(__dirname,'../templates')
 // Specify the path to the public directory
 const publicPath = path.join(__dirname, "../public"); 
 
+// Configure session middleware
+app.use(
+    session({
+      secret: "your-secret-key",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
 
+  
 app.use(express.json())
 app.set("view engine","hbs")
 app.set("views", templatePath)
@@ -37,23 +51,46 @@ app.post("/signup",async (req,res)=>{
     res.render("home")
 })
 
-app.post("/login",async (req,res)=>{
+app.post("/login", async (req, res) => {
+    try {
+      const check = await collection.findOne({ name: req.body.name });
+      if (check) {
+        if (check.password === req.body.password) {
+          // Store user data in session
+          req.session.user = check;
+  
+          res.redirect("/home");
+        } else {
+          res.send("Password is incorrect");
+        }
+      } else {
+        res.send("Username or password does not exist");
+      }
+    } catch (error) {
+      res.send("An error occurred");
+    }
+  });
 
-    try{
-        const check=await collection.findOne({name:req.body.name})
-        if(check.password===req.body.password){
-            res.render("home");
-        }
-        else{
-            res.send("Password is incorrect")
-        }
+// Home route with authentication check
+app.get("/home", (req, res) => {
+    if (req.session.user) {
+      res.render("home");
+    } else {
+      res.redirect("/");
     }
-    catch{
-        res.send("Username or password does not exist")
+  });
+  
+
+app.get("/logout", (req, res) => {
+// Destroy the session to log out the user
+req.session.destroy((err) => {
+    if (err) {
+        console.log(err);
     }
+        res.redirect("/");
+    });
+});
     
-})
-
 app.listen(PORT, ()=>{
     console.log(`server started on ${PORT}`);
 })
